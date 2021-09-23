c9b8a0df67c79772ed4be159bf904d2e9f543dc2
Jianping <jshen92@wisc.edu>
2021-09-23 17:57:40 -0500

    Initialize memA memB

---
+++ ccip_mmio/hw/memA.sv
@@ -0,0 +1,11 @@
+module memA
+  #(
+    parameter BITS_AB=8,
+    parameter DIM=8
+    )
+   (
+    input                      clk,rst_n,en,WrEn,
+    input signed [BITS_AB-1:0] Ain [DIM-1:0],
+    input [$clog2(DIM)-1:0] Arow,
+    output signed [BITS_AB-1:0] Aout [DIM-1:0]
+   );
