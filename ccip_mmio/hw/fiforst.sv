f9fd1e8b623eb5517248c2f25d182430782bf824
Jianping <jshen92@wisc.edu>
2021-09-23 17:53:50 -0500

    add fiforst

---
+++ ccip_mmio/hw/fiforst.sv
@@ -0,0 +1,41 @@
+// fiforst.sv
+// Implements delay buffer (fifo)
+// On reset all entries are set to 0
+// Shift causes fifo to shift out oldest entry to q, shift in d
+// Unsyncronized rst and syncronized rst
+
+module fiforst
+  #(
+  parameter DEPTH=8,
+  parameter BITS=64
+  )
+  (
+  input clk,rst_n,en,rst,
+  input [BITS-1:0] d,
+  output [BITS-1:0] q
+  );
+  // your RTL code here
+  logic [BITS-1:0] data [DEPTH - 1: 0];
+  integer i;
+  always_ff @(posedge clk, negedge rst_n) begin
+	if(!rst_n) begin
+		for (i = 0; i < DEPTH; i++) begin
+			data[i] <= 0;
+		end
+	end
+    else if(rst) begin
+		for (i = 0; i < DEPTH; i++) begin
+			data[i] <= 0;
+		end
+	end
+	else if(en) begin
+		for (i = 0; i < DEPTH - 1; i++) begin
+			data[i] <= data[i+1];
+		end
+		data[DEPTH-1] <= d;
+	end
+  end
+  
+  assign q = data[0];
+  
+endmodule // fifo
