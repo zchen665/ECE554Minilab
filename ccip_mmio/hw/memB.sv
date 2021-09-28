e1078b96186901bed125b45f2b4532b3377c13e4
tyee2 <tyee2@wisc.edu>
2021-09-28 17:16:58 -0500

    Update memB.sv

--- ccip_mmio/hw/memB.sv
+++ ccip_mmio/hw/memB.sv
@@ -9,8 +9,7 @@ module memB
     output signed [BITS_AB-1:0] Bout [DIM-1:0]
     );
 	
-    logic signed [BITS_AB-1:0] Btmp [DIM-1:0];
-	logic cnt;
+	logic signed [DIM-1:0] cnt;
 	
     // Only for test memB_tb
 	genvar i;
@@ -20,19 +19,22 @@ module memB
 				.clk(clk),
 				.rst_n(rst_n),
 				.en(en),
-				.d(Bin[i]),
-				.q(Btmp[i])
+				.d(DIM > cnt ? Bin[i] : {DIM{1'b0}}),
+				.q(Bout[i])
 			);
 		end
 	endgenerate
-/* 	
+ 	
 	always @(posedge clk, negedge rst_n) begin
 		if(!rst_n)
 			cnt <= 0;
-		else
+		else if(!en)
+			cnt <= 0;
+		else if (cnt <= DIM)
 			cnt <= cnt + 1;
+		
 	end
-	
+/*	
 	// parallelogram shift
 	always_comb begin
 		Bout[0] = Btmp[0];
